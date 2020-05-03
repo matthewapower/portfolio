f95f6cccd02bcfe0b5adeec28343cc886ae589ed
matthewapower <matthewallenpower@gmail.comm>
2020-05-02 22:44:58 -0400

    Add in favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,18 @@ module.exports = {
       options: {},
     },
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `matthewapower`,
+        short_name: `mpower`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
